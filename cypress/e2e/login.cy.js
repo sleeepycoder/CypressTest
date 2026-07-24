c2538fa052226d4f149c76f19e372bba959c5245
PANKAJ SINGH CHAUHAN <96768418+sleeepycoder@users.noreply.github.com>
2026-07-24 15:00:08 +0530

    update code

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,6 @@
 import { user } from '../fixtures/userData.js';
 import LoginPage from '../pages/LoginPage.js';
 describe('Register User', () => {
-
     beforeEach(() => {
         cy.visit('/'); // Replace with your registration page URL
     });
@@ -9,6 +8,7 @@ describe('Register User', () => {
     it('should register a new user and preserve their data', () => {
         cy.get("a[href='/login']").should('be.visible').click();
         LoginPage.registerUser(user.firstName, user.email);
+        cy.contains('Enter Account Information', { timeout: 20000 }).should('be.visible');
         LoginPage.SignUpDetails(user.password);
         cy.get('[data-qa="account-created"]').should('be.visible').should('contain', 'Account Created!');
         cy.get('[data-qa="continue-button"]').click();
@@ -18,7 +18,6 @@ describe('Register User', () => {
     });
 
     it('should login with the saved user data', () => {
-         cy.get("a[href='/login']").should('be.visible').click();
-      
+        cy.get("a[href='/login']").should('be.visible').click();
     });
 });
