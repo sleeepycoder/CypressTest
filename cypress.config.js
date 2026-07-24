e8755c73a93333c03519765135be1f9dcde2553a
PANKAJ SINGH CHAUHAN <96768418+sleeepycoder@users.noreply.github.com>
2026-07-24 14:47:47 +0530

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,42 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  video: true,
+  screenshotOnRunFailure: true,
+
+  reporter: "cypress-mochawesome-reporter",
+
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    overwrite: false,
+    html: true,
+    json: true,
+  },
 
   e2e: {
-    video: true,
     baseUrl: "https://automationexercise.com/",
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/e2e.js",
-    viewportHeight: 1080,
-    viewportWidth: 1920,
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 60000,
-    requestTimeout: 15000,
-    responseTimeout: 15000,
-    retries: {
-      runMode: 2,
-      openMode: 0,
-    },
-    reporter: "cypress-mochawesome-reporter",
-    reporterOptions: {
-      reportDir: "cypress/reports/mochawesome-report",
-      charts: true,
-      reportPageTitle: "Automation Exercise Test Report",
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      overwrite: false,
-      html: true,
-      json: true, 
-    },
     setupNodeEvents(on, config) {
-      // register mochawesome reporter plugin
-      try {
-        require('cypress-mochawesome-reporter/plugin')(on);
-      } catch (e) {
-      
-        throw e;
-      }
+      require("cypress-mochawesome-reporter/plugin")(on);
       return config;
     },
   },
