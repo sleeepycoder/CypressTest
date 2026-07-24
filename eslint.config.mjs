7173b397405a23780276fd1aa6afd3e5d5f18852
PANKAJ SINGH CHAUHAN <96768418+sleeepycoder@users.noreply.github.com>
2026-07-24 15:27:34 +0530

    test

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from "globals";
+import eslintPluginCypress from "eslint-plugin-cypress";
+
+export default [
+  {
+    files: ["cypress/**/*.js"],
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+
+    plugins: {
+      cypress: eslintPluginCypress,
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "cypress/no-unnecessary-waiting": "warn",
+      "cypress/assertion-before-screenshot": "error",
+      "cypress/no-force": "warn",
+      "cypress/no-assigning-return-values": "error"
+    },
+  },
+];
